ffb763471b20fdec28c594835b7928a316facd57
XinTong717 <xtong717@gmail.com>
2026-01-30 12:59:14 -0800

    cookie

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,6 +1,14 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+type CookieStore = Awaited<ReturnType<typeof cookies>>
+type CookieToSet = {
+  name: string
+  value: string
+  options?: Parameters<CookieStore['set']>[2]
+}
+
+
 export async function createClient() {
   const cookieStore = await cookies()
 
@@ -12,7 +20,7 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: CookieToSet[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
