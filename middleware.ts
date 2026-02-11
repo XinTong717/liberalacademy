59e7d745310e0e3703b5eb74fae8acb6249d229f
XinTong717 <xtong717@gmail.com>
2026-02-11 12:02:18 -0800

    middleware

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl
+
+  if (pathname.startsWith('/_AMapService/')) {
+    const rewriteUrl = req.nextUrl.clone()
+    rewriteUrl.pathname = pathname.replace('/_AMapService/', '/api/amap/')
+    return NextResponse.rewrite(rewriteUrl)
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/_AMapService/:path*'],
+}
